289c28f91ed78e5dff2a90b6b666e1acd728d6f4
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-21 14:42:14 -0300

    Fixed wrong property assigment on config

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ const aws = require('aws-sdk');
 
 /* READING VARS FROM HEROKU ENVIRONMENT */
 let s3 = new aws.S3({
-  apiBaseUrl = process.env.API_BASE_URL,
+  apiBaseUrl: process.env.API_BASE_URL,
   apiRequiredHeaders: process.env.API_REQUIRED_HEADERS,
   port: process.env.PORT,
   logLevel: process.env.LOG_LEVEL,
@@ -16,9 +16,11 @@ let s3 = new aws.S3({
 const config = {
     port: s3.port || process.env.PORT,
     logLevel: s3.logLevel || process.env.LOG_LEVEL,
-    apiBaseUrl = s3.apiBaseUrl || process.env.API_BASE_URL,
-    apiRequiredHeaders = JSON.parse(s3.apiRequiredHeaders || process.env.API_REQUIRED_HEADERS),
+    apiBaseUrl: s3.apiBaseUrl || process.env.API_BASE_URL,
+    apiRequiredHeaders: JSON.parse(s3.apiRequiredHeaders || process.env.API_REQUIRED_HEADERS),
     allowedOrigins: s3.allowedOrigins || process.env.ALLOWED_ORIGINS
 };
 
-export default config;
+module.exports = {
+    config
+}
